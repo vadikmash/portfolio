238e5f3149965b7bfb626e57ef5a467038842f7f
Vadzim Mashnitski <vadikmash@gmail.com>
2025-01-10 14:17:32 +0100

    feat: setup footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,10 +15,9 @@
 			{@render children()}
 		</main>
 
-		<footer>
-			<p>
-				visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-			</p>
+		<footer class="footer">
+			<div>© 2024—2025</div>
+			<a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA</a>
 		</footer>
 	</div>
 </ParaglideJS>

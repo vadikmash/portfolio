c97a0a7ba1f081094b66b811134b04e1a998807b
Vadzim Mashnitski <vadikmash@gmail.com>
2025-01-16 16:14:26 +0100

    feat: improve home page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,24 +7,28 @@
 </svelte:head>
 
 <section>
-	<p class="mt-3 text-2xl font-light">
-		I'm a software engineer, creator, musician. I specialize in web/mobile apps full-stack
-		development. When not busy with my main job, I create my own cool apps and services, write
-		songs, travel.
-	</p>
+	<section class="mt-3 flex flex-col gap-x-12 gap-y-6 md:flex-row">
+		<p class="text-2xl font-light md:basis-2/3">
+			I'm a software engineer, creator, musician. I specialize in full-stack development of web and
+			mobile apps. When not busy with my main job, I create cool apps and services of my own, write
+			songs, travel.
+		</p>
 
-	<div>
-		<ul>
-			<li><a href="mailto:vadikmash@gmail.com">vadikmash@gmail.com</a></li>
-			<li><a href="https://github.com/vadikmash">GitHub</a></li>
-			<li><a href="https://linedin.com/in/vadikmash">LinkedIn</a></li>
-			<li><a href="https://x.com/vadikmash">X (Twitter)</a></li>
-		</ul>
-	</div>
+		<div class="md:mt-0 md:basis-1/3">
+			<ul class="flex flex-wrap gap-x-3 gap-y-1 md:flex-col">
+				<li><a href="mailto:vadikmash@gmail.com">vadikmash@gmail.com</a></li>
+				<li><a href="https://github.com/vadikmash">GitHub</a></li>
+				<li><a href="https://linedin.com/in/vadikmash">LinkedIn</a></li>
+				<li><a href="https://x.com/vadikmash">X (Twitter)</a></li>
+			</ul>
+		</div>
+	</section>
 
-	<h2>Tech Projects</h2>
+	<section class="mt-10">
+		<h2 class="mt-5 text-3xl font-bold">Tech Projects</h2>
 
-	<h2>Music</h2>
+		<h2 class="mt-5 text-3xl font-bold">Music</h2>
 
-	<h2>Wanna hire me? Check out my CV</h2>
+		<h2 class="mt-5 text-3xl font-bold">Wanna hire me? Check out my CV</h2>
+	</section>
 </section>

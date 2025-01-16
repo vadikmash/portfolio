809b9667bb994a9d029e7e16283ed803230bcc88
Vadzim Mashnitski <vadikmash@gmail.com>
2025-01-16 12:17:34 +0100

    feat: improve nav links, extract into dedicated component

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,29 +1,14 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import NavLink from './NavLink.svelte';
 </script>
 
 <header class="flex w-full justify-center p-6 pb-10">
-	<nav class="flex justify-between w-full">
+	<nav class="flex w-full justify-between">
 		<ul class="flex gap-4">
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				{#if $page.url.pathname === '/' }
-					<h1>
-						<a href="/" class="font-bold text-black shadow-none">Vadzim Mashnitski</a>
-					</h1>
-				{:else}
-					<a href="/">Vadzim Mashnitski</a>
-				{/if}
-			</li>
-			<li aria-current={$page.url.pathname.includes('/projects') ? 'page' : undefined}>
-				<a href="/projects">Projects</a>
-			</li>
-			<li aria-current={$page.url.pathname.includes('/cv') ? 'page' : undefined}>
-				<a href="/cv">CV</a>
-			</li>
-			<li aria-current={$page.url.pathname.includes('/blog') ? 'page' : undefined}>
-				<a href="/blog">Blog</a>
-			</li>
+			<NavLink label="Vadzim Mashnitski" path="/" isHeader />
+			<NavLink label="Projects" path="/projects" />
+			<NavLink label="CV" path="/cv" />
+			<NavLink label="Blog" path="/blog" />
 		</ul>
 	</nav>
-	
 </header>
---
+++ src/routes/NavLink.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	const { label, path, isHeader = false } = $props();
+</script>
+
+<li aria-current={page.url.pathname === path ? 'page' : undefined}>
+	{#if page.url.pathname === path && isHeader}
+		<h1>
+			<a href={path} class="border-none font-bold text-current">{label}</a>
+		</h1>
+	{:else if page.url.pathname === path}
+		<a href={path} class="border-none font-bold text-current">{label}</a>
+	{:else}
+		<a href={path}>{label}</a>
+	{/if}
+</li>

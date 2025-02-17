097ed5954638c0b32036004acd74838b6050d572
Vadzim Mashnitski <vadikmash@gmail.com>
2025-02-17 21:12:51 +0300

    feat: add avatar to navigation

--- src/routes/components/Header/Header.svelte
+++ src/routes/components/Header/Header.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import NavLink from './components/NavLink.svelte';
+	import avatarSource from '$lib/images/avatar.jpg';
 </script>
 
 <header class="flex w-full justify-center py-6">
 	<nav class="flex w-full justify-between">
-		<ul class="flex gap-4 md:gap-6">
-			<NavLink label="Vadzim Mashnitski" path="/" isHeader />
+		<ul class="flex items-center gap-4 md:gap-6">
+			<NavLink
+				label="Vadzim Mashnitski"
+				path="/"
+				isHeader
+				imageSource={avatarSource}
+				imageAlt="avatar"
+			/>
 			<NavLink label="Projects" path="/projects" />
 			<NavLink label="CV" path="/cv" />
 			<NavLink label="Blog" path="/blog" />
--- src/routes/components/Header/components/NavLink.svelte
+++ src/routes/components/Header/components/NavLink.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
 	import { page } from '$app/state';
 
-	const { label, path, isHeader = false } = $props();
+	const { label, path, isHeader = false, imageSource = '', imageAlt = '' } = $props();
 </script>
 
 <li aria-current={page.url.pathname === path ? 'page' : undefined}>
 	{#if page.url.pathname === path && isHeader}
-		<h1>
-			<a href={path} class="border-none font-bold text-current">{label}</a>
+		<h1 class="flex items-center justify-center">
+			{#if imageSource}
+				<a href={path} class="border-none">
+					<img src={imageSource} alt={imageAlt} class="mr-2 h-9 w-9 rounded-full" />
+				</a>
+			{/if}
+			<a href={path} class="border-none font-bold text-current">
+				{label}
+			</a>
 		</h1>
-	{:else if page.url.pathname === path}
-		<a href={path} class="border-none font-bold text-current">{label}</a>
 	{:else}
-		<a href={path}>{label}</a>
+		<span class="flex items-center justify-center">
+			{#if imageSource}
+				<a href={path} class="border-none">
+					<img src={imageSource} alt={imageAlt} class="mr-2 h-9 w-9 rounded-full" />
+				</a>
+			{/if}
+			<a href={path} class={page.url.pathname === path ? 'border-none font-bold text-current' : ''}>
+				{label}
+			</a>
+		</span>
 	{/if}
 </li>

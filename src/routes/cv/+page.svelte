67f5751ba71fc594b7c1e233f1d7b363112875e7
Vadzim Mashnitski <vadikmash@gmail.com>
2025-02-17 21:12:59 +0300

    feat: add cv sections, fill in the summary, improve layout

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -3,6 +3,13 @@
 	import avatarSource from '$lib/images/avatar.jpg';
 
 	let { data }: { data: PageData } = $props();
+
+	const today = new Date();
+	const beginningOfCareer = new Date(2019, 0, 15);
+	const millisecondsInYear = 1000 * 60 * 60 * 24 * 365;
+	const yearsOfExperience = Math.floor(
+		(today.getTime() - beginningOfCareer.getTime()) / millisecondsInYear
+	);
 </script>
 
 <svelte:head>
@@ -10,28 +17,52 @@
 	<meta name="description" content="Vadzim Mashnitski's CV" />
 </svelte:head>
 
-<section class="flex flex-wrap items-start justify-between gap-y-5">
-	<div class="flex items-center gap-5">
-		<img
-			src={avatarSource}
-			alt="Vadzim Mashnitski"
-			class="-ml-0 h-[100px] w-[100px] rounded-full"
-		/>
-		<div class="flex flex-col justify-center">
-			<h2 class="mb-2 text-2xl font-bold">Vadzim Mashnitski</h2>
-			<h3 class="font-thin text-gray-400">Full-Stack Software Engineer</h3>
+<section
+	class="mt-3 flex flex-wrap items-start justify-between gap-5 border-b border-gray-200 pb-6"
+>
+	<div class="md:basis-2/3">
+		<div class="flex items-center gap-5">
+			<img
+				src={avatarSource}
+				alt="Vadzim Mashnitski"
+				class="-ml-0 h-[70px] w-[70px] rounded-full"
+			/>
+			<div class="flex flex-col items-center">
+				<h2 class="text-2xl font-bold">Vadzim Mashnitski</h2>
+				<h3 class="mt-1 font-thin text-gray-400">Full-Stack Software Engineer</h3>
+			</div>
 		</div>
 	</div>
-	<div class="pr-20 md:pt-5">
-		<h4 class="text-xl font-bold">Contact info</h4>
 
-		<div class="mt-3 md:basis-1/3">
-			<ul class="flex flex-wrap gap-x-3 gap-y-1 md:flex-col">
-				<li><a href="mailto:vadikmash@gmail.com">vadikmash@gmail.com</a></li>
-				<li><a href="https://github.com/vadikmash">GitHub</a></li>
-				<li><a href="https://linkedin.com/in/vadikmash">LinkedIn</a></li>
-				<li><a href="https://x.com/vadikmash">X (Twitter)</a></li>
-			</ul>
-		</div>
+	<div>
+		<ul class="flex flex-wrap gap-x-3 gap-y-1 md:flex-col md:items-end">
+			<li><a href="mailto:vadikmash@gmail.com">vadikmash@gmail.com</a></li>
+			<li><a href="https://github.com/vadikmash">GitHub</a></li>
+			<li><a href="https://linkedin.com/in/vadikmash">LinkedIn</a></li>
+			<li><a href="https://x.com/vadikmash">X (Twitter)</a></li>
+		</ul>
 	</div>
+</section>
+
+<section class="mt-5 border-b border-gray-200 pb-6">
+	<ul class="flex flex-col gap-3">
+		<li>
+			<strong>Summary:</strong> Professional Full Stack Software Engineer with more than {yearsOfExperience}
+			years of web & mobile development experience.
+		</li>
+		<li><strong>Years of experience:</strong> {yearsOfExperience}+</li>
+		<li><strong>English:</strong> Advanced (C1)</li>
+	</ul>
+</section>
+
+<section class="mt-5 border-b border-gray-200 pb-6">
+	<h4 class="text-xl font-bold">Technical Skills</h4>
+</section>
+
+<section class="mt-5 border-b border-gray-200 pb-6">
+	<h4 class="text-xl font-bold">Professional experience</h4>
+</section>
+
+<section class="mt-5 flex flex-wrap items-start justify-between gap-y-5">
+	<h4 class="text-xl font-bold">Education</h4>
 </section>

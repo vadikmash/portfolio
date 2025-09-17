aa64782cb1036be2f83ef701331eb1e330beca34
Vadzim Mashnitski <vadikmash@gmail.com>
2025-09-17 20:30:55 +0300

    feat: change self-intro wording (yet again)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,8 @@
 <section class="mt-3">
 	<section class="flex flex-col gap-x-12 gap-y-6 md:flex-row">
 		<p class="text-3xl md:basis-2/3">
-			I'm a software engineer building quality & impactful products, Web3 enthusiast. I enjoy making
-			music, traveling, creating.
+			A software engineer building quality & impactful products. Web3 enthusiast. Making music,
+			traveling, creating.
 		</p>
 
 		<div class="md:basis-1/3">
